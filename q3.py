63aab39e4ab32c734ef4aec8efa6badb4fbc4aee
Ugaso <ugasosheikabdi@gmail.com>
2014-12-07 22:17:47 -0800

    solved prob3

--- q3.py
+++ q3.py
@@ -1,27 +1,42 @@
 #Q3: Largest prime factor
-import math
+import math, types
 
 def primes_sieve(num):
-	prime_divisors = []
+	temp_primes = []
+	remove = []
+	upper_limit = int(math.ceil(num**.5))
 
 	if num < 2:
 		return 'no prime numbers found'
-	elif num == 2:
-		return num,' is prime, with no other divisors'
-	elif num == 3:
-		return num,' is prime, with no other divisors'
 	else:
 		#find all divisors
-		for i in range(2, int(math.ceil(num**.5))+2):
+		for i in range(2, upper_limit):
+
 			#check for divsors
 			if num % i == 0:
-				print i, 'potential prime'
-				#check for more primes
-				if num/i
-			#check which divisors are prime
+				temp_primes.append(i)
 
-		#return all prime divisors in an array
+		#check for more divisors	
+		for n in temp_primes:
+			divisor = num/n
+			if divisor not in temp_primes:
+				temp_primes.append(divisor)
+			
+		for n in temp_primes:
+			for i in temp_primes:
+				if n % i == 0 and n != i:
+					if n not in remove:
+						remove.append(n)
 
-	return 'end of program'
+		for n in remove:
+			if n in temp_primes:
+				temp_primes.remove(n)
+
+
+		if len(temp_primes) == 0:
+			return num, 'is potentially a prime number'
+
+		else:
+			return 'all the prime divisors of ', num, ' are ', temp_primes
 
 print primes_sieve(14)
